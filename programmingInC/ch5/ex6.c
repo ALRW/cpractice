fadee346c21dc8962d374a62a499f21cee865eda
ALRW <awerner1@googlemail.com>
2017-10-08 08:39:54 +0100

    [AW] finished ch5 exercises

--- programmingInC/ch5/ex6.c
+++ programmingInC/ch5/ex6.c
@@ -3,7 +3,7 @@
 
 int main(void){
 
-        int input, number, right_digit, divisor;
+        int input, number, right_digit, divisor, output;
         bool isNegative = false;
 
         printf("Enter your number... \n");
@@ -12,7 +12,7 @@ int main(void){
         if(number < 0){
                 isNegative = true;
                 number = -number;
-                printf("minus");
+                printf("minus ");
         }
 
         if(number == 0){
@@ -21,7 +21,29 @@ int main(void){
                 for(divisor = 1; divisor <= number; divisor *= 10);
                 do{
                         divisor /= 10;
-                        printf("%d", number / divisor);
+                        output = number / divisor;
+                        if(output == 1)
+                                printf("one ");
+                        else if(output == 2)
+                                printf("two ");
+                        else if(output == 3)
+                                printf("three ");
+                        else if(output == 4)
+                                printf("four ");
+                        else if(output == 5)
+                                printf("five ");
+                        else if(output == 6)
+                                printf("six ");
+                        else if(output == 7)
+                                printf("seven ");
+                        else if(output == 8)
+                                printf("eight ");
+                        else if(output == 9)
+                                printf("nine ");
+                        else if(output == 0)
+                                printf("zero ");
+                        else
+                                printf("Error: bad divisor");
                         number %= divisor;
                 }while(number);
         }
