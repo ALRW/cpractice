033486df079aac2800c1e92bf8745193821712cd
ALRW <awerner1@googlemail.com>
2017-10-20 11:09:55 +0100

    [AW] some refinements to substring

--- programmingInC/ch9/ex4.c
+++ programmingInC/ch9/ex4.c
@@ -2,18 +2,27 @@
 
 void substring(const char input[], int start, int length, char output[])
 {
-        for(int i = 0;i < length; ++start, ++i)
+        int i;
+
+        for(i = 0;i < length; ++start, ++i)
         {
+                if(input[start] == '\0')
+                        break;
+
                 output[i] = input[start];
         }
+        output[i] = '\0';
 }
 
 int main (void)
 {
         void substring(const char input[], int start, int length, char output[]);
-        char result[50];
+        char result[50], result2[50];
+
 
         substring("character", 4, 3, result);
+        substring("two words", 4, 20, result2);
         printf("%s\n", result);
+        printf("%s\n", result2);
 }
 
